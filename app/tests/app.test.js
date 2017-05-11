dd566581ffdff1a41d2696546d9f699bb76b482c
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-11 17:06:21 +0530

    add automatic tests using Karma

---
+++ app/tests/app.test.js
@@ -0,0 +1,8 @@
+const expect = require('expect')
+
+// categorizing tests using describe function
+describe('App', () => {
+  it('should properly run tests', () => {
+    expect(1).toBe(1)
+  })
+})
